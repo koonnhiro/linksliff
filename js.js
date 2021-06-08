102941bdfb8c55f76cbfb33e2e06cd0bc634ad71
koonnhiro <78940480+koonnhiro@users.noreply.github.com>
2021-06-08 15:58:14 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 <script>
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1656040664-WaRdlzml";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
