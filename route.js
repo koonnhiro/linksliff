988673c6ebfc6b85fa7849c83eb36185a31a594d
koonnhiro <78940480+koonnhiro@users.noreply.github.com>
2021-06-14 16:12:34 +0900

    Update route.js

--- route.js
+++ route.js
@@ -12,9 +12,19 @@ $(function () {
         var type = $('input[name="type"]:checked').val();
         var from = $('input[name="from"]').val();
         var to = $('input[name="to"]').val();
+        var price = $('input[name="price"]').val();
+        var sum;
 
-        var msg = `日付：${date}\n区分：${type}\n出発：${from}\n到着：${to}`;
-        sendText(msg);
+        // 往復の場合
+        if (type === "往復") {
+            sum = price * 2;
+        } else {
+            sum = price;
+        }
+
+        var msg = `日付：${date}\n区分：${type}\n出発：${from}\n到着：${to}\n金額：${price}円\n合計：${sum}円`;
+        //sendText(msg);
+        console.log(msg);
 
         return false;
     });
