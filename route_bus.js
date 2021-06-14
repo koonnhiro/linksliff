9abfb76d6fc95df00939214f8096e86a48185ff8
koonnhiro <78940480+koonnhiro@users.noreply.github.com>
2021-06-14 17:26:09 +0900

    Create route_bus.js

---
+++ route_bus.js
@@ -0,0 +1,31 @@
+$(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
+    // 送信
+    $('form').submit(function () {
+        var date = $('input[name="date"]').val();
+        var type = $('input[name="type"]:checked').val();
+        var from = $('input[name="from"]').val();
+        var to = $('input[name="to"]').val();
+        var price = $('input[name="price"]').val();
+        var sum;
+
+        // 往復の場合
+        if (type === "往復") {
+            sum = price * 2;
+        } else {
+            sum = price;
+        }
+
+        var msg = `【バス】\n日付：${date}\n区分：${type}\n出発：${from}\n到着：${to}\n金額：${price}円\n合計：${sum}円`;
+        sendText(msg);
+        //console.log(msg);
+
+        return false;
+    });
+});
