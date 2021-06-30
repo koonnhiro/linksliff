c89aa80c39a85453c8d47e86402c4f97c71d8211
koonnhiro <78940480+koonnhiro@users.noreply.github.com>
2021-06-30 15:19:40 +0900

    Update route_car.js

--- route_car.js
+++ route_car.js
@@ -12,7 +12,8 @@ $(function () {
         var type = $('input[name="type"]:checked').val();
         var from = $('input[name="from"]').val();
         var to = $('input[name="to"]').val();
-        var price = $('input[name="distance"]').val() * 10;
+        var distance = $('input[name="distance"]').val();
+        var price = distance * 15;
         var sum;
 
         // 往復の場合
@@ -22,7 +23,7 @@ $(function () {
             sum = price;
         }
 
-        var msg = `【車】\n日付：${date}\n区分：${type}\n出発：${from}\n到着：${to}\n金額：${price}円\n合計：${sum}円`;
+        var msg = `【車】\n日付：${date}\n区分：${type}\n出発：${from}\n到着：${to}\n距離：${distance}\n金額：${price}円\n合計：${sum}円`;
         sendText(msg);
         //console.log(msg);
 
