5bb8ee1ed0bf66e4a0bbad3ac78356e1b2b24721
koonnhiro <78940480+koonnhiro@users.noreply.github.com>
2022-01-25 23:01:28 +0900

    Update location.js

--- location.js
+++ location.js
@@ -51,3 +51,17 @@ function shareTargetPicker(text) {
         window.alert('Failed to send message ' + error);
     });
 }
+
+$(function () {
+
+    // 送信
+    $('form').submit(function () {
+        var client = $('input[name="client"]').val();
+        var anken = $('input[name="anken"]').val();
+
+        var msg = `【位置情報】\nクライアント名：${client}\n案件名：${anken}`;
+        sendText(msg);
+
+        return false;
+    });
+});
